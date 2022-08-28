afc01a7ab348630e7ca74ae470be595495673794
Youssef OUBAIDA <youssefoubaida1992@gmail.com>
2022-08-28 15:52:44 +0100

    last version

--- src/App.js
+++ src/App.js
@@ -6,15 +6,26 @@ import Footer from "./Components/Footer";
 import Loading from "./Components/Loading";
 import { useState, useEffect } from "react";
 function App() {
+  const [load, setload] = useState(true);
   const [menuClick, setMenu] = useState(false);
-
+  useEffect(() => {
+    setload(true);
+    setTimeout(() => {
+      setload(false);
+    }, 2500);
+  }, []);
   return (
     <div className="App">
-      
+      {load ? (
+        <Loading />
+      ) : (
+        <>
           <Nav menuClick={menuClick} setMenu={setMenu}/>
           <Header menuClick={menuClick}/>
          <Main />
           <Footer />
+        </>
+      )}
     </div>
   );
 }
